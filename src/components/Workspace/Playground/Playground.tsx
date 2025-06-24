62c54abf25c8a652f0e9516a881ef6b30b163d9d
Abhiram <55952410+Afterlifegamer@users.noreply.github.com>
2025-06-24 20:44:40 +0530

    Added a two-sum problem

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -4,12 +4,16 @@ import Split from "react-split";
 import CodeMirror from "@uiw/react-codemirror";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import { javascript } from "@codemirror/lang-javascript";
+import EditorFooter from "./EditorFooter";
 
 type PlaygroundProps = object;
 
 const Playground: React.FC<PlaygroundProps> = () => {
+  const boilerPlate = `function twoSums{
+    //write your code here
+  };`;
   return (
-    <div className="flex flex-col bg-dark-layer-1 relative">
+    <div className="flex flex-col bg-dark-layer-1 relative overflow-x-hidden">
       <PreferenceNav />
       <Split
         className="h-[calc(100vh-94px)]"
@@ -19,23 +23,57 @@ const Playground: React.FC<PlaygroundProps> = () => {
       >
         <div className="w-full h-full overflow-auto ">
           <CodeMirror
-            value="const a=1;"
+            value={boilerPlate}
             theme={vscodeDark}
             extensions={[javascript()]}
             style={{ fontSize: 16 }}
           ></CodeMirror>
         </div>
-        <div className="w-full overflow-auto px-5">
-          <div className="flex h-10 items-center space-x-6">
+        <div className="w-full z-1 overflow-scroll px-5 ">
+          <div className="flex flex-col space-y-2">
             <div className="relative flex h-full flex-col justify-center cursor-pointer">
-              <div className="text-sm font-medium leading-5 text-white">
+              <div className="text-xl font-medium leading-5 text-white mb-2 mt-2">
                 Testcases
+                <hr className="absolute bottom-0 h-0.5 w-20 rounded-full border-none bg-white" />
               </div>
-              <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white" />
+            </div>
+            <div className="flex">
+              <div className="mr-2 text-start text-white mt-2">
+                <div className="flex flex-wrap items-center-gap-4">
+                  <div className="font-medium items-center bg-dark-fill-3 transition-all rounded-lg px-4 py-1 hover:bg-dark-fill-2 relative cursor-pointer whitespace-nowrap inline-flex">
+                    Case 1
+                  </div>
+                </div>
+              </div>
+              <div className="mr-2 text-start text-white mt-2">
+                <div className="flex flex-wrap items-center-gap-4">
+                  <div className="font-medium items-center bg-dark-fill-3 transition-all rounded-lg px-4 py-1 hover:bg-dark-fill-2 relative cursor-pointer whitespace-nowrap inline-flex">
+                    Case 2
+                  </div>
+                </div>
+              </div>
+              <div className="mr-2 text-start text-white mt-2">
+                <div className="flex flex-wrap items-center-gap-4">
+                  <div className="font-medium items-center bg-dark-fill-3 transition-al l rounded-lg px-4 py-1 hover:bg-dark-fill-2 relative cursor-pointer whitespace-nowrap inline-flex">
+                    Case 3
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="font-semibold my-4">
+            <p className="text-sm font-medium text-white">Input:</p>
+            <div className="text-white cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent mt-2 mb-2">
+              nums: [2,7,9,10] target: 9
+            </div>
+            <p className="text-sm font-medium text-white">Output:</p>
+            <div className="text-white cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent mt-2">
+              [0,1]
             </div>
           </div>
         </div>
       </Split>
+      <EditorFooter />
     </div>
   );
 };
